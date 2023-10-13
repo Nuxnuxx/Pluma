ac91605142fa82a90d4f823fe28e5ecf625830bf
elisechabeniuk <elise.chabeniuk@etud.u-picardie.fr>
2023-10-13 11:18:32 +0200

    annulation

--- backend/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const UserController = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/', UserController.getAllUsers);
-router.post('/', UserController.addUser);
-
-module.exports = router;
