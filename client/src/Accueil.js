03ed232508f00bba448d20b8483994a3c3646af6
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-10-13 13:50:46 +0200

    Ajout du background

--- client/src/Accueil.js
+++ client/src/Accueil.js
@@ -1,9 +1,11 @@
 import './styles/App.css';
-//import {UserList} from "./components/UserList/userListComponent";
 import Header from "./components/Header/Header";
+import './styles/Accueil.scss';
+
 export default function Home() {
     return (
-        <Header></Header>
-    )
+        <div>
+            <Header />
+        </div>
+    );
 }
-
