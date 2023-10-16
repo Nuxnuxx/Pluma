4ac1eab2ce91a3cd399c17d428b68ac5f151448d
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-10-16 13:07:09 +0200

    Elements centrés

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.scss'; // Assurez-vous d'importer votre fichier SCSS
 function Header() {
     return (
         <div className="header-container">
-            <div className="logo-and-text">
+            <div className="logo-et-texte">
                 <img src="/assets/plume-doie.png" alt="logo clair" className="logo" />
                 <h5>Pluma</h5>
             </div>
