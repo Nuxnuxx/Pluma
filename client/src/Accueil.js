32920a88e518c3123bfa85ed34dca5f0d808fc22
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-10-16 11:43:55 +0200

    Ajout logo centrale

--- client/src/Accueil.js
+++ client/src/Accueil.js
@@ -1,12 +1,14 @@
 import './styles/App.css';
 import Header from "./components/Header/Header";
 import './styles/Accueil.scss';
+import React from "react";
 
 export default function Home() {
     return (
         <div>
             <Header />
             <div className="w-screen h-screen flex flex-wrap flex-col justify-center items-center">
+                <img src="/assets/logo_clair_1.png" alt="logo clair 1" className="logoaccueil" />
                 <h1>Pluma</h1>
                 <h3>Laissez votre créativité prendre son envol.</h3>
                 <div>
