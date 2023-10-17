1d44902a017a4cc8637293fd8a566739385d4ef8
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-10-17 11:23:32 +0200

    Finition de la page d'accueil (sans responsive)

--- client/src/siteVitrine/pages/Accueil.js
+++ client/src/siteVitrine/pages/Accueil.js
@@ -4,12 +4,14 @@ import "../styles/Accueil.scss"
 export default function Accueil() {
     return (
         <div>
-            <div className="flex flex-wrap flex-col justify-center items-center">
-                <img src="/assets/logo_clair_1.png" alt="logo pluma clair" className="logoaccueil" />
-                <h1 className="plumaText">Pluma</h1>
+            <div className="flex flex-col justify-center items-center">
+                <div className="partie-principale-hero">
+                    <img src="/assets/logo_clair_1.png" alt="logo pluma clair" className="logoaccueil mx-auto" />
+                    <h1 className="plumaText">Pluma</h1>
+                </div>
                 <div className="slogan">Laissez votre créativité prendre son envol</div>
                 <div className="mt-5">
-                    <button className="btnAccueil mr-56">S'inscrire</button>
+                    <button className="btnAccueil mr-4">S'inscrire</button>
                     <button className="btnAccueil">Se connecter</button>
                 </div>
             </div>
