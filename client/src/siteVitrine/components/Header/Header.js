6483c4215a6b1879f655ef4df35a9a611d323f4d
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-10-17 10:35:48 +0200

    Amélioration du Header du site vitrine

--- client/src/siteVitrine/components/Header/Header.js
+++ client/src/siteVitrine/components/Header/Header.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import './Header.scss';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+    const url = useLocation();
+
     return (
         <div className="header-container">
             <div className="logo-et-texte">
-                <img src="/assets/plume-doie.png" alt="logo clair" className="logo" />
-                <h5>Pluma</h5>
+                <Link to="/" className={url.pathname === '/' ? 'active' : ''}>
+                    <img src="/assets/plume-doie.png" alt="logo clair" className="logo" />
+                    <h5>Pluma</h5>
+                </Link>
             </div>
-            <div className="menu"> {/* Ajout d'une classe "menu" */}
+            <div className="menu">
                 <nav>
-                    <ul className="menu-ul"> {/* Ajout d'une classe "menu-ul" */}
-                        <li>
-                            <Link to="/">Accueil</Link>
-                        </li>
-                        <li>
-                            <Link to="/qui-sommes-nous">Qui sommes nous</Link>
-                        </li>
-                        <li>
-                            <Link to="/premium">PlumaPremium</Link>
-                        </li>
-                        <li>
-                            <Link to="/inscription-connexion">Accéder à votre espace</Link>
-                        </li>
+                    <ul className="menu-ul">
+                        <Link to="/" className={url.pathname === '/' ? 'active' : ''}>
+                            <li>
+                                Accueil
+                            </li>
+                        </Link>
+                        <Link to="/qui-sommes-nous" className={url.pathname === '/qui-sommes-nous' ? 'active' : ''}>
+                            <li>
+                                Qui sommes nous
+                            </li>
+                        </Link>
+                        <Link to="/premium" className={url.pathname === '/premium' ? 'active' : ''}>
+                            <li>
+                                PlumaPremium
+                            </li>
+                        </Link>
+                        <Link to="/inscription-connexion" className={url.pathname === '/inscription-connexion' ? 'active a-acces-espace' : 'a-acces-espace'}>
+                            <li>
+                                Accéder à votre espace
+                            </li>
+                        </Link>
                     </ul>
                 </nav>
             </div>
