f12b2abe0394dc063595dd5674d4f5dcdcc4e334
FOUQUEROLLE Thomas <fouquerollethomas@gmail.com>
2023-10-17 21:06:40 +0200

    - Ajout d'images | - Mise en place de ReactFlow | - Conception de la toolbar

---
+++ client/src/siteOutil/components/Toolbar/toolbar.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import './toolbar.scss';
+
+const Toolbar = () => {
+    const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
+    const openOverlay = () => {
+        setIsOverlayOpen(true);
+    };
+
+    const closeOverlay = () => {
+        setIsOverlayOpen(false);
+    };
+
+    const onDragStart = (event, nodeType) => {
+        event.dataTransfer.setData('application/reactflow', nodeType);
+        event.dataTransfer.effectAllowed = 'move';
+    };
+
+    return (
+        <div className="toolbar relative z-10 w-auto bg-white h-screen flex flex-col justify-between">
+            <div className="content">
+                <div className="dndnode chapitre mb-6">
+                    <img src="/assets/toolbar/chapitre.png" alt="Icone chapitre" title="Chapitre" onDragStart={(event) => onDragStart(event, 'chapitre')} draggable />
+                </div>
+                <div className="dndnode acte mb-6">
+                    <img src="/assets/toolbar/acte.png" alt="Icone acte" title="Acte" onDragStart={(event) => onDragStart(event, 'acte')} draggable />
+                </div>
+                <div className="dndnode personnage mb-6">
+                    <img src="/assets/toolbar/personnage.png" alt="Icone personnage" onDragStart={(event) => onDragStart(event, 'personnage')} draggable />
+                </div>
+                <div className="dndnode lieu mb-6">
+                    <img src="/assets/toolbar/lieu.png" alt="Icone lieu" onDragStart={(event) => onDragStart(event, 'personnage')} draggable />
+                </div>
+                <div className="dndnode evenement mb-6">
+                    <img src="/assets/toolbar/evenement.png" alt="Icone evenement" onDragStart={(event) => onDragStart(event, 'evenement')} draggable />
+                </div>
+                <div className="dndnode bloc-note">
+                    <img src="/assets/toolbar/bloc-note.png" alt="Icone bloc-note" title="Bloc-note" onDragStart={(event) => onDragStart(event, 'bloc-note')} draggable />
+                </div>
+            </div>
+
+            <div className="generateur-nom flex items-center justify-center">
+                <button onClick={openOverlay}>
+                    <img src="/assets/toolbar/eclair.png" alt="Icone générateur de nom (éclair)" draggable="false"/>
+                </button>
+            </div>
+            {isOverlayOpen && (
+                <div className="overlay">
+                    <button onClick={closeOverlay} className="button">X</button>
+                </div>
+            )}
+
+        </div>
+    );
+};
+
+export default Toolbar;
--- client/src/siteOutil/pages/EditionProjet.js
+++ client/src/siteOutil/pages/EditionProjet.js
@@ -1,6 +1,64 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import ReactFlow, {
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+import '../styles/EditionProjet.scss'
+import Toolbar from '../components/Toolbar/toolbar';
 
-const EditionProjet = () => {
-    return <h1>EditionProjet</h1>;
-};
+const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-export default EditionProjet;
+export default function EditionProjet() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const isScreenTooSmall = screenWidth < 1023; // Exemple : seuil de 768 pixels
+
+
+    return (
+        <div>
+        {isScreenTooSmall ? (
+            <p>La taille de l'écran n'est pas adaptée pour afficher cette page.</p>
+        ) : <div style={{ width: '87vw', height: '100vh' }}>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    fitView
+                >
+                    <Toolbar />
+                    <Controls position="top-right" />
+
+                    <MiniMap />
+                    <Background variant="dots" gap={10} size={0.5} />
+                </ReactFlow>
+            </div>}
+        </div>
+    );
+}
