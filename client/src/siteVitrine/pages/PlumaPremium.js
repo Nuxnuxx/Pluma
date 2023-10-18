e8f1c9672b87c0f8af2f4f85d330eb081e13d5a2
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-10-18 17:23:44 +0200

    Avancement page Premium

--- client/src/siteVitrine/pages/PlumaPremium.js
+++ client/src/siteVitrine/pages/PlumaPremium.js
@@ -3,8 +3,10 @@ import "../styles/PlumaPremium.scss";
 
 const PlumaPremium = () => {
     return (
-        <img src="/assets/pile-de-livre.png" alt="pile de livre" className="livre-de-livre"/>
-        
+        <div>
+            <img src="/assets/pile-de-livre.png" alt="pile de livre" className="livre-de-livre"/>
+            <h1>PlumaPremium</h1>
+        </div>
     );
 };
 
