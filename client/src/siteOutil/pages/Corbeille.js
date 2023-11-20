57b8a3af19c0c9f998c58a2a70b4c41efc2fc164
Martin <martin.gotz@etud.u-picardie.fr>
2023-11-20 12:15:51 +0100

    Avancée des pages

--- client/src/siteOutil/pages/Corbeille.js
+++ client/src/siteOutil/pages/Corbeille.js
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import "../styles/StyleMonEspace.scss"
 import "../styles/StyleCorbeille.scss"
+import ElementListeProjets from "../components/elementListeProjets/elementListeProjets";
 
 const Corbeille = () => {
     const elementsDansCorbeille= [
@@ -18,6 +20,16 @@ const Corbeille = () => {
         console.log("Restauration de l'élément " + elementId);
     };
 
+    const [expandedItems, setExpandedItems] = useState([]);
+
+    const toggleExpansion = (itemId) => {
+        setExpandedItems((prevExpandedItems) =>
+            prevExpandedItems.includes(itemId)
+                ? prevExpandedItems.filter((id) => id !== itemId)
+                : [...prevExpandedItems, itemId]
+        );
+    };
+
     return (
         <div className="mon-espace">
             <div className="corbeille">
@@ -27,11 +39,18 @@ const Corbeille = () => {
                 ) : (
                     <ul>
                         {elementsDansCorbeille.map((element) => (
-                            <li key={element.id}>
-                                <span>Projet {element.id}</span>
-                                <div className="boutons">
-                                    <button className="bouton bouton-restaurer" onClick={() => onRestore(element.id)}>Restaurer</button>
-                                    <button className="bouton bouton-supprimer" onClick={() => onDelete(element.id)}>Supprimer</button>
+                            <li key={element.id} className={expandedItems.includes(element.id) ? 'expanded' : ''}
+                                onClick={() => toggleExpansion(element.id)}>
+                                <div className="header-element" >
+                                    <span>Projet {element.id}</span>
+                                    <div className="boutons">
+                                        <button className="bouton bouton-restaurer" onClick={() => onRestore(element.id)}>Restaurer</button>
+                                        <button className="bouton bouton-supprimer" onClick={() => onDelete(element.id)}>Supprimer</button>
+                                    </div>
+                                </div>
+                                <div className="details-element">
+                                    <ElementListeProjets id={element.id} color="blue" onClick={() => {}} />
+                                    <p>Informations supplémentaires...</p>
                                 </div>
                             </li>
                         ))}
