85e64c5dc815868aa532548677f925d6b911ea87
Elise_CHABENIUK <90648568+LiliChab@users.noreply.github.com>
2023-11-21 14:17:03 +0100

    Update PlumaPremium.js correction orthographe

--- client/src/siteVitrine/pages/PlumaPremium.js
+++ client/src/siteVitrine/pages/PlumaPremium.js
@@ -18,7 +18,7 @@ const PlumaPremium = () => {
             </div>
             <div className="pricingTable mx-auto mt-10">
                 <h2 className="pricingTable-title">Trouve la plume qui te convient.</h2>
-                <h3 className="pricingTable-subtitle">Soyez Organisé et créatif pour seulement 7,99€ par mois.</h3>
+                <h3 className="pricingTable-subtitle">Soyez organisé et créatif pour seulement 7,99€ par mois.</h3>
 
                 <ul className="pricingTable-firstTable">
                     <li className="pricingTable-firstTable_table">
@@ -26,16 +26,16 @@ const PlumaPremium = () => {
                         <p className="pricingTable-firstTable_table__pricing">
                             <span>€</span><span>0</span><span>par mois</span></p>
                         <ul className="pricingTable-firstTable_table__options first_table">
-                            <li>Nombre de blocs illimités</li>
+                            <li>Nombre de blocs illimité</li>
                             <li>Limitation à 5 projets</li>
-                            <li>Personnalisation des lieux et personnages</li>
-                            <li>10 générations hebdomadaire de nom pour personnage</li>
+                            <li>Personnalisation des lieux et des personnages</li>
+                            <li>10 générations hebdomadaires de nom pour personnage</li>
                             <li>Sauvegarde automatique</li>
                             <li>Accès à 3 designs pour l’espace de travail</li>
                             <li>Exportation des histoires</li>
                             <li>Correcteur d’orthographe et de grammaire</li>
                             <li>Possibilité de varier entre une unique police serif et sans-serif</li>
-                            <li>Panel d’image prédéfni pour les avatars des personnages et les lieux</li>
+                            <li>Panel d’images prédéfini pour les avatars des personnages et les lieux</li>
                         </ul>
                     </li>
                     <li className="pricingTable-firstTable_table">
@@ -43,18 +43,18 @@ const PlumaPremium = () => {
                         <p className="pricingTable-firstTable_table__pricing">
                             <span>€</span><span>7,99</span><span>par mois</span></p>
                         <ul className="pricingTable-firstTable_table__options second_table">
-                            <li>Nombre de blocs illimités</li>
-                            <li>Création de projets en illimités</li>
-                            <li>Personnalisation des lieux et personnages</li>
-                            <li>Génération de nom pour personnage illimité</li>
+                            <li>Nombre de blocs illimité</li>
+                            <li>Création de projets en illimitée</li>
+                            <li>Personnalisation des lieux et des personnages</li>
+                            <li>Génération de nom pour personnage illimitée</li>
                             <li>Sauvegarde automatique</li>
                             <li>Accès à plus de 20 designs pour l’espace de travail</li>
                             <li>Exportation des histoires</li>
                             <li>Correcteur d’orthographe et de grammaire</li>
                             <li>Plus de 50 polices d’écritures différentes</li>
-                            <li>Panel d’image prédéfni pour les avatars des personnages et les lieux</li>
+                            <li>Panel d’images prédéfni pour les avatars des personnages et les lieux</li>
                             <li>Accès au résumé par IA</li>
-                            <li>Génération d'image par IA pour les avatars des personnages et les lieux</li>
+                            <li>Génération d'images par IA pour les avatars des personnages et les lieux</li>
                         </ul>
                         <button className="pricingTable-firstTable_table__getstart">Essayez Maintenant !</button>
                     </li>
@@ -63,16 +63,16 @@ const PlumaPremium = () => {
                         <p className="pricingTable-firstTable_table__pricing">
                             <span>€</span><span>0</span><span>par mois</span></p>
                         <ul className="pricingTable-firstTable_table__options third_table">
-                            <li>Nombre de blocs illimités</li>
+                            <li>Nombre de blocs illimité</li>
                             <li>Limitation à 10 projets</li>
-                            <li>Personnalisation des lieux et personnages</li>
-                            <li>Génération de nom pour personnage illimité</li>
+                            <li>Personnalisation des lieux et des personnages</li>
+                            <li>Génération de noms pour personnage illimitée</li>
                             <li>Sauvegarde automatique</li>
                             <li>Accès à plus de 20 designs pour l’espace de travail</li>
                             <li>Exportation des histoires</li>
                             <li>Correcteur d’orthographe et de grammaire</li>
                             <li>10 polices d’écritures différentes</li>
-                            <li>Panel d’image prédéfni pour les avatars des personnages et les lieux</li>
+                            <li>Panel d’images prédéfni pour les avatars des personnages et les lieux</li>
                             <li>Accès au résumé par IA</li>
                         </ul>
                         <button className="pricingTable-firstTable_table__getstart">Essayez Maintenant !</button>
@@ -83,13 +83,13 @@ const PlumaPremium = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-center items-center  m-16">
                 <div className="text-center">
-                    <strong>Faite 37% d'économie sur l'année en prenant l'abbonnement PlumaPremium Annuel pour seulement 59,99€ par an</strong>
+                    <strong>Faite 37% d'économie sur l'année en prenant l'abonnement PlumaPremium Annuel pour seulement 59,99€ par an</strong>
                 </div>
                 <div className="text-center">
-                    <strong>Profiter d'une expérience sans publicité pour que vous ne soyez plus intérompu</strong>
+                    <strong>Profitez d'une expérience sans publicité pour que vous ne soyez plus interrompu</strong>
                 </div>
                 <div className="text-center">
-                    <strong>Vos notes sont partout et vos écrits sont baclés ? Rejoignez PlumaPremium afin de profiter de toute les fonctionnalités possible.</strong>
+                    <strong>Vos notes sont partout et vos écrits sont baclés ? Rejoignez PlumaPremium afin de profiter de toutes les fonctionnalités possibles.</strong>
                 </div>
             </div>
             <div className="text-center">
