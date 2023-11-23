71b7a17ffef3775be6a9e480cab16f272efcee10
Elise_CHABENIUK <90648568+LiliChab@users.noreply.github.com>
2023-11-23 19:04:28 +0100

    tentative de connection mariadb

---
+++ server/models/BDD.js
@@ -0,0 +1,33 @@
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host : 'localhost',
+//   port : 3307,
+//   user : 'anais',
+//   password : 'dev',
+//   database : 'pluma'
+// });
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: "127.0.0.1",
+     user:"anais", 
+     password: "dev",
+     database : "pluma",
+     socketPath : "/run/mysqld/mysqld.sock"
+});
+
+module.exports = {
+  pool,
+  async query(sql, values) {
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      return await conn.query(sql, values);
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) conn.end();
+    }
+  }
+};
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UserController = require('./controllers/userController');
+const pool = require('./models/BDD');
 
 const router = express.Router();
 
@@ -8,6 +9,17 @@ router.post('/', UserController.addUser);
 
 router.get('/api/mon-espace', (req, res) => {
     // Gérer la route /api/mon-espace ici
+});
+
+router.get('/bdd', async (req, res) => {
+  try {
+    const rows = await query("SELECT * FROM abonnement");
+    console.log(rows);
+    res.status(200).json(rows);  // Envoyer les données JSON à la réponse HTTP
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur lors de la requête à la base de données');
+  }
 });
 
 module.exports = router;
