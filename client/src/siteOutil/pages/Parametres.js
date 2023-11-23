5fdca3dedd1e02ce4ff77d2d83f25654b44f1ea4
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-11-23 11:19:32 +0100

    Changement de l'image via un bouton - Page Paramètres

--- client/src/siteOutil/pages/Parametres.js
+++ client/src/siteOutil/pages/Parametres.js
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 import "../styles/Parametres.scss";
 
 const Parametres = () => {
-    const idUser = 1;
+    const [image, setImage] = useState("../assets/profil.png");
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
         <div className="parametres">
             <h1 className="titre">Profil</h1>
+            <div>
+                <img src={image} alt="Profil" />
+                <input type="file" onChange={handleImageChange} accept="image/*" />
+            </div>
         </div>
     );
 };
