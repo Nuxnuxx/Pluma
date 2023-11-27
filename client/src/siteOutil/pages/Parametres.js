5a80eb329e5b2aa28a7da031874104de73f7c2bf
ballesterloryne@gmail.com <ballesterloryne@gmail.com>
2023-11-27 11:20:51 +0100

    Page 404 + design terminée

--- client/src/siteOutil/pages/Parametres.js
+++ client/src/siteOutil/pages/Parametres.js
@@ -135,7 +135,6 @@ const Parametres = () => {
                         </svg>
                         <p className="ms-4">Se déconnecter</p>
                     </button>
-
                     <button className="flex">
                         <svg id="delete-account" width="24" height="24" fill="#ff564f" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2ZM4 12c0-4.42 3.58-8 8-8 1.85 0 3.55.63 4.9 1.69L5.69 16.9A7.902 7.902 0 0 1 4 12Zm3.1 6.31A7.902 7.902 0 0 0 12 20c4.42 0 8-3.58 8-8 0-1.85-.63-3.55-1.69-4.9L7.1 18.31Z" clipRule="evenodd"></path>
