989ee0108d0a4f7ac7293e7a487c80abc67b74f1
Elise_CHABENIUK <90648568+LiliChab@users.noreply.github.com>
2023-11-27 15:59:53 +0100

    Update db.js suppression des commentaires inutiles et ajout en commentaire du pool pour le serveur

--- server/models/db.js
+++ server/models/db.js
@@ -1,38 +1,3 @@
-// const mysql = require('mysql');
-
-// const connection = mysql.createConnection({
-//   host : 'localhost',
-//   port : 3307,
-//   user : 'anais',
-//   password : 'dev',
-//   database : 'pluma'
-// });
-
-/*
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
-     host: "127.0.0.1",
-     user:"anais", 
-     password: "dev",
-     database : "pluma",
-     socketPath : "/run/mysqld/mysqld.sock"
-});
-
-module.exports = {
-  pool,
-  async query(sql, values) {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      return await conn.query(sql, values);
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) conn.end();
-    }
-  }
-}*/
-
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: '127.0.0.1',
@@ -41,7 +6,14 @@ const pool = mariadb.createPool({
     password: 'coucou123',
     database: 'pluma',
 });
-
+/* Pool pour le serveur de prod
+const pool = mariadb.createPool({
+     host: "mariadb",
+     user:"anais", 
+     password: "dev",
+     database : "pluma",
+});
+*/
 module.exports = {
     pool,
     async query(sql, values) {
